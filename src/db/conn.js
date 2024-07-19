10beaff19067fab5b1c68ae55e86e9b66957d62d
samuel.victor <samuel.victor@ebserh.gov.br>
2024-07-19 18:01:00 -0300

    log db conn

--- src/db/conn.js
+++ src/db/conn.js
@@ -6,7 +6,7 @@ async function main () {
         mongoose.set('strictQuery', true)
         await mongoose.connect(process.env.DEV_NAME === 'RenderMidnightDEPLOY' ? process.env.PROD_CONNECTION_STRING : process.env.CONNECTION_STRING )
             .then(() => {
-                console.log(`\n✨✨✨✨✨✨✨✨✨✨\n\n⭐ Bem-vindo de volta, ${process.env.DEV_NAME}! 🌃\n🗓️  ` + new Date().toLocaleString('pt-BR') + `⏱️\n\nConexão com MongoDB estabelecida com Sucesso\n\n🎫🎟️🎫🎟️🎫🎟️🎫🎟️🎫🎟️🎫🎟️\n`)
+                console.log(`\n✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨\n\n\t🌜 MIDNIGHTICKETS 🌛\n\n⭐ Bem-vindo de volta, ${process.env.DEV_NAME}! 🌃\n🗓️  ` + new Date().toLocaleString('pt-BR') + `⏱️\n\n🏦🎲 Conexão com MongoDB estabelecida com Sucesso em Modo ${process.env.DEV_NAME === 'RenderMidnightDEPLOY' ? 'Produção 🌍🌎' : 'Desenvolvimento 🔩🛠️'}\n\n🎫🎟️🎫🎟️🎫🎟️🎫🎟️🎫🎟️🎫🎟️\n\n✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨\n`)
             })
             .catch((error) => {
                 console.error('Connection error: ' + error)
