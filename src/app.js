5e752bb8bdfe37aea66775e78d80548466070ff0
Pedro Rodrigues <pedro.rodrigues@tradexsolutions.com.br>
2024-07-23 23:50:43 -0300

    Starting adding admin-bro

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const router = require('./routes/router');
 const cors = require('cors');
 const conn = require('./db/conn');
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
+const { default: mongoose } = require('mongoose');
+const apiRouter = require('./routes/router');
+
 const app = express();
 
-conn()
+conn();
+
+const adminBro = new AdminBro({
+  databases: [mongoose],
+  rootPath: '/admin',
+});
+
+const adminRouter = AdminBroExpress.buildRouter(adminBro);
+
 app.use(cors());
 app.use(express.json());
-app.use('/api', router);
 
-module.exports = app;   
+app.use('/api', apiRouter);
+app.use(adminBro.options.rootPath, adminRouter);
+
+module.exports = app;
