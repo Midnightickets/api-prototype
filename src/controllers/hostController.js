5e99aea20d7bac1df537b67b5aac438d91fdd861
samuel.victor <samuel.victor@ebserh.gov.br>
2024-07-30 12:39:15 -0300

    loginhost

--- src/controllers/hostController.js
+++ src/controllers/hostController.js
@@ -1,3 +1,4 @@
+const { SuccessEnum } = require("../enums/Enums");
 const HostManager = require("../managers/HostManager");
 
 const hostController = {
@@ -5,6 +6,15 @@ const hostController = {
         await HostManager.createHost(req.body)
             .then((host) =>{
                 res.status(201).json(host)
+            })
+            .catch((error) => {
+                res.status(400).json({ error: error.message });
+            });
+    },
+    login_host: async (req, res) => {
+        await HostManager.loginHost(req.body)
+            .then((host) =>{
+                res.status(200).json({host,  message: SuccessEnum.LOGIN_SUCCESS });
             })
             .catch((error) => {
                 res.status(400).json({ error: error.message });
--- src/routes/hostRouter.js
+++ src/routes/hostRouter.js
@@ -4,4 +4,7 @@ const  hostController = require('../controllers/hostController');
 router.route('/create_host')
     .post((req, res) => hostController.criar_host(req, res));
 
+router.route('/login_host')
+    .post((req, res) => hostController.login_host(req, res));
+    
 module.exports = router
