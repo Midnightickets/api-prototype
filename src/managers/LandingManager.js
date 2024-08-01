df56738c8edfa28a8ed70a4de7d661b5860cd758
samuel.victor <samuel.victor@ebserh.gov.br>
2024-07-31 21:04:34 -0300

    twilio wpp integracao new form

--- src/managers/LandingManager.js
+++ src/managers/LandingManager.js
@@ -1,14 +1,20 @@
-const { ErrorEnum } = require('../enums/Enums');
-const { Landing: LandingModel } = require('../models/Landing');
+const { ErrorEnum } = require("../enums/Enums");
+const { Landing: LandingModel } = require("../models/Landing");
 
 const LandingManager = {
-    saveEmail: async (body) => {
-        if(typeof body.email !== 'string' || body.email.length == 0 || body.email.length > 255 || !body.email.includes('@')) {
-            throw new Error(ErrorEnum.INVALID_EMAIL);
-        }
-        const landing = new LandingModel(body);
-        return await landing.save();
+  saveEmail: async (body) => {
+    if (
+      typeof body.email !== "string" ||
+      body.email.length == 0 ||
+      body.email.length > 255 ||
+      !body.email.includes("@")
+    ) {
+      throw new Error(ErrorEnum.INVALID_EMAIL);
     }
-}
+
+    const landing = new LandingModel(body);
+    return await landing.save();
+  },
+};
 
 module.exports = LandingManager;
